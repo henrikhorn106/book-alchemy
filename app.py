b36c9c1226ca6c2fb8e280b31e6f8cc4ec4e40ee
Henrik Horn <henrik.horn106@gmail.com>
2025-10-22 19:13:20 +0200

    Added add_book, add_author and table feature

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
+from datetime import datetime
+
+from flask import Flask, render_template, request
 
 from data_models import db, Author, Book
-from flask import Flask
 
 # Initialize flask app
 app = Flask(__name__)
@@ -13,6 +15,51 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data
 # Connect database to flask app
 db.init_app(app)
 
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method == 'POST':
+        # Get data from form
+        name = request.form['name']
+        birth_date = datetime.strptime(request.form['birthdate'], '%Y-%m-%d').date()
+        try:
+            date_of_death = datetime.strptime(request.form['date_of_death'], '%Y-%m-%d').date()
+        except ValueError:
+            date_of_death = None
+
+        # Add data to database
+        author = Author(name=name, birth_date=birth_date, date_of_death=date_of_death)
+        db.session.add(author)
+        db.session.commit()
+        return f"Author {name} added successfully!"
+
+    else:
+        return render_template("add_author.html")
+
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        # Get data from form
+        isbn = request.form['isbn']
+        title = request.form['title']
+        publication_year = request.form['publication_year']
+        author_id = request.form['author_id']
+
+        # Add data to database
+        book = Book(isbn=isbn, title=title, publication_year=publication_year, author_id=author_id)
+        db.session.add(book)
+        db.session.commit()
+        return f"Book {title} added successfully!"
+
+    else:
+        return render_template("add_book.html", authors=Author.query.all())
+
+
+@app.route('/')
+def index():
+    return render_template("home.html", books=Book.query.all())
+
 # Create tables in database
 # with app.app_context():
-#  db.create_all()
+#     db.create_all()
--- data_models.py
+++ data_models.py
@@ -6,7 +6,7 @@ class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     birth_date = db.Column(db.Date)
-    dater_of_death = db.Column(db.Date)
+    date_of_death = db.Column(db.Date)
 
     def __repr__(self):
         return f'<Author {self.name}>'
