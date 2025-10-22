86a8b6a69043b88bde2f9b5754125f90d8436d3d
Henrik Horn <henrik.horn106@gmail.com>
2025-10-22 17:42:32 +0200

    Connect flask app with database

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+import os
+
+from data_models import db, Author, Book
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
+# Initialize flask app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.sqlite'
-db = SQLAlchemy(app)
+
+# Initialize database
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
+
+# Connect database to flask app
+db.init_app(app)
